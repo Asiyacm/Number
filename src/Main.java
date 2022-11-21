d11ed6471938c900c3ceccb15e6662ce2aceb69b
Asiyacm <asiyacmuhammed@gmail.com>
2022-11-21 21:45:04 +0530

    ch commit

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
         int option;
         do {
-            System.out.println("Choose the opertion");
+            System.out.println("Choose the option");
             System.out.println("\n1.Largest\n2.Smallest\n3.Prime\n4.Even or not\n5.Reverse\n6.Exit");
+            System.out.println("Enter your choice:");
             Scanner xy = new Scanner(System.in);
             option = xy.nextInt();
             switch (option) {
@@ -47,50 +48,51 @@ public class Main {
 
                     System.out.println("Enter the Number");
                     float n = xy.nextInt();
-                    float ab = n/2;
-                    if(ab==0||ab==1){
-                        System.out.println(n+" is not prime number");
-                    }else{
-                        for(int i=2;i<=ab;i++){
-                            int flag;
-                            if(n%i==0){
-                                System.out.println(n+" is not prime number");
-                               flag=1;
+                    float ab = n / 2;
+                    float flag = 0, z = 0;
+                    z = n / 2;
+                    if (z == 0 || z == 1) {
+                        System.out.println(n + " is not prime number");
+                    } else {
+                        for (int i = 2; i <= z; i++) {
+                            if (n % i == 0) {
+                                System.out.println(n + " is not prime number");
+                                flag = 1;
                                 break;
-ver                            }
-
-                        else{
-                            System.out.println(n+" is prime number");
                             }
+                        }
+                        if (flag == 0) {
+                            System.out.println(n + " is prime number");
                         }
                     }
 
                     break;
+
                 case 4:
 
                     System.out.println("Enter the Number");
                     float p = xy.nextInt();
-                    if(p % 2 == 0){
+                    if (p % 2 == 0) {
                         System.out.println("Even number");
-                    }else {
+                    } else {
                         System.out.println("Not Even number");
                     }
-                                break;
-                    case 5:
-
-                        System.out.println("Enter the Number");
-                        int r = xy.nextInt();
-                        int rem;
-                        int rev=0;
-                        while(r > 0)
-                        {
-                            rem = r % 10;
-                            rev = (rev * 10) + rem;
-                            r = r/10;
-                        }
-                        System.out.println("The reverse of the given number is: " + rev);
-                        break;
+                    break;
                 case 5:
+
+                    System.out.println("Enter the Number");
+                    int r = xy.nextInt();
+                    int rem;
+                    int rev = 0;
+                    while (r > 0) {
+                        rem = r % 10;
+                        rev = (rev * 10) + rem;
+                        r = r / 10;
+                    }
+                    System.out.println("The reverse of the given number is: " + rev);
+                    break;
+
+                case 6:
 
                     System.out.println("Enter exit");
                     break;
@@ -100,11 +102,7 @@ ver                            }
                     break;
 
             }
-
-
-
-        }while (option != 6);
-
+        } while (option != 6);
 
     }
 }
